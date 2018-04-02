ac8245b8e24871885c883f47b8ae5f3b69dc93c4
Bubel <a.bubel@itransition.corp>
2018-04-02 18:47:42 +0300

    Still in progress

--- MusicPortal.Web/Startup.cs
+++ MusicPortal.Web/Startup.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MusicPortal.DAL.EF;
+using MusicPortal.DAL.Interfaces;
+using MusicPortal.DAL.Repositories;
 
 namespace MusicPortal.Web {
     public class Startup {
@@ -17,6 +17,11 @@ namespace MusicPortal.Web {
         public IConfiguration Configuration { get; }
         
         public void ConfigureServices(IServiceCollection services) {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ApplicationContext"), b => b.MigrationsAssembly("MusicPortal.DAL")));
+
             services.AddMvc();
         }
         

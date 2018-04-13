9fe9a30425412d87545c68b1c82360944e67fafe
Anton Bubel <a.bubel@itransition.corp>
2018-04-13 13:46:30 +0300

    tests done, all works great

---
+++ MusicPortal.BLL.UnitTests/ApplicationContextBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPortal.DAL.EF;
+
+namespace MusicPortal.BLL.UnitTests {
+    class ApplicationContextBuilder {
+        private static readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=MusicPortalDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+        private readonly ApplicationContext _context;
+        
+        public ApplicationContextBuilder() {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlServer(connectionString, b => b.MigrationsAssembly("MusicPortal.DAL"))
+                .Options;
+            _context = new ApplicationContext(dbOptions);
+        }
+
+        public ApplicationContext GetApplicationContext() {
+            return _context;
+        }
+    }
+}
--- MusicPortal.BLL.UnitTests/ArtistServiceTest.cs
+++ MusicPortal.BLL.UnitTests/ArtistServiceTest.cs
@@ -8,17 +8,13 @@ using MusicPortal.BLL.Interfaces;
 using MusicPortal.BLL.DTO;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace MusicPortal.BLL.UnitTests {
     public class ArtistServiceTest {
-        private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=MusicPortalDb;Trusted_Connection=True;MultipleActiveResultSets=true";
-
         private IArtistService GetArtistService() {
-            var dbOptions = new DbContextOptionsBuilder<ApplicationContext>()
-                .UseSqlServer(connectionString, b => b.MigrationsAssembly("MusicPortal.DAL"))
-                .Options;
             return new ArtistService(
-                new UnitOfWork(new ApplicationContext(dbOptions)), 
+                new UnitOfWork(new ApplicationContextBuilder().GetApplicationContext()), 
                 new Mapper(new MapperConfiguration(cfg => {
                     cfg.AddProfile(new AutoMapperProfile());
                 }))
@@ -29,22 +25,22 @@ namespace MusicPortal.BLL.UnitTests {
         [InlineData(1, 40)]
         [InlineData(5, 20)]
         [InlineData(6, 44)]
-        public async void GetTopArtists_PaginationTest_ReturnsLessOrEqualAmountOfItems(int page, int itemsPerPage) {
+        public async Task GetTopArtists_PaginationTest_ReturnsLessOrEqualAmountOfItems(int page, int itemsPerPage) {
             IArtistService artistService = GetArtistService();
 
-            List<ArtistDto> artists = await artistService.GetTopArtists(page, itemsPerPage);
+            var artists = await artistService.GetTopArtists(page, itemsPerPage);
 
-            Assert.False(artists.Count > 44, "collection size is greater than expected");
+            Assert.False(artists.Count > itemsPerPage, "collection size is greater than expected");
         }
 
         [Theory]
         [InlineData("Bones")]
         [InlineData("Lil Peep")]
         [InlineData("Surrenderdorothy")]
-        public async void GetSimilarArtists_ResponseTest_ReturnsNotEmptyCollection(string name) {
+        public async Task GetSimilarArtists_ResponseTest_ReturnsNotEmptyCollection(string name) {
             IArtistService artistService = GetArtistService();
 
-            List<ArtistDto> artists = await artistService.GetSimilarArtists(name);
+            var artists = await artistService.GetSimilarArtists(name);
 
             Assert.NotEmpty(artists);
         }
